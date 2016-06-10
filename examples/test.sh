3e6a0ccfed264b77b12858c5bda17a71776867ea
Splizard <splizard@splizard.com>
2016-06-10 22:22:15 +1200

    Pretty testing

--- examples/test.sh
+++ examples/test.sh
@@ -2,34 +2,46 @@
 
 #Test A+B
 cd A+B/ && ic a+b.i && uct -py a+b.u
+if [ "$?" -eq "1" ]; then
+	echo -e "A+B \e[31mFAILED!\e[0m to compile D:"
+	exit 1
+fi
 OUTPUT=$(python3 a+b.py <<< $(echo -e "2 2\n"))
 if [ "$OUTPUT" = "4" ]; then
-	echo "A+B PASSED!"
+	echo -e "A+B \e[32mPASSED!\e[0m"
 else
-	echo "A+B FAILED!"
+	echo -e "A+B \e[31mFAILED!\e[0m"
 	exit 1
 fi
 
 #Test Arithmetic
 cd ../Arithmetic/ && ic Arithmetic.i && uct -py Arithmetic.u
+if [ "$?" -eq "1" ]; then
+	echo -e "Arithmetic \e[31mFAILED!\e[0m to compile D:"
+	exit 1
+fi
 OUTPUT=$(python3 Arithmetic.py <<< $(echo -e "3 5\n"))
 DEFINED=$(echo -e "Sum: 8\nDifference: -2\nProduct: 15\nQuotient: 0\nModulus: 3\nExponent: 243")
 if [ "$OUTPUT" = "$DEFINED" ]; then
-	echo "Arithmetic PASSED!"
+	echo -e "Arithmetic \e[32mPASSED!\e[0m!"
 else
-	echo "Arithmetic FAILED!"
+	echo -e "Arithmetic \e[31mFAILED!\e[0m!"
 	echo "$OUTPUT"
 	exit 1
 fi
 
 function BasicTest {
 	cd ../$1/ && ic $1.i && uct -py $1.u
+	if [ "$?" -eq "1" ]; then
+		echo -e "$1 \e[31mFAILED!\e[0m to compile D:"
+		exit 1
+	fi
 	local OUTPUT=$(python3 $1.py)
 	local DEFINED=$(echo -e "$2")
 	if [ "$OUTPUT" = "$DEFINED" ]; then
-		echo "$1 PASSED!"
+		echo -e "$1 \e[32mPASSED!\e[0m"
 	else
-		echo "$1 FAILED! Got:"
+		echo -e "$1 \e[31mFAILED!\e[0m Got:"
 		echo	 "$OUTPUT"
 		echo "(Expecting)"
 		echo	 "$DEFINED"
