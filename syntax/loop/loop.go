e2b8f6f6ee2f9377b52edf3494b80c69dc4f553e
Quentin Quaadgras <quentin@splizard.com>
2018-05-08 14:06:31 +1200

    Multi-level break.

--- syntax/loop/loop.go
+++ syntax/loop/loop.go
@@ -2,6 +2,7 @@ package loop
 
 import "github.com/qlova/ilang/syntax/symbols"
 import "github.com/qlova/uct/compiler"
+import "github.com/qlova/ilang/types/number"
 
 var Name = compiler.Translatable{
 	compiler.English: "loop",
@@ -12,8 +13,19 @@ var Flag = compiler.Flag {
 	
 	OnLost: func(c *compiler.Compiler) {
 		c.Redo()
+		
+		if _, ok := c.Scope[len(c.Scope)-2].Flags[Name[c.Language]]; ok {
+			c.Copy()
+			c.If()
+				c.Int(1)
+				c.Sub()
+				c.Done()
+			c.No()
+			c.Drop()
+		}
 	},
 }
+
 
 var Statement = compiler.Statement {
 	Name: Name,
@@ -35,3 +47,29 @@ var End = compiler.Statement {
 	},
 }
 
+var Break = compiler.Statement {
+	Name: compiler.Translatable{
+		compiler.English: "break",
+	},
+	 
+	OnScan: func(c *compiler.Compiler) {
+		
+		if c.Peek() != "\n" {
+			c.ExpectingType(number.Type)
+			
+			if _, ok := c.Scope[len(c.Scope)-2].Flags[Name[c.Language]]; ok {
+				c.Int(1)
+				c.Sub()
+			}
+			c.Done()
+			
+			return
+		}
+
+		if _, ok := c.Scope[len(c.Scope)-2].Flags[Name[c.Language]]; ok {
+			c.Int(0)
+		}
+		
+		c.Done()
+	},
+}
